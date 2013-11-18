b9f27fd75dccea501adef898e02b5bf4f4ab5105
Adrien Fallou <adrien.fallou@gmail.com>
2013-11-17 22:19:36 -0800

    Added missing argument for function

--- regression_code/oximetry_linear_regression.m
+++ regression_code/oximetry_linear_regression.m
@@ -1,9 +1,9 @@
-function oximetry_linear_regression(training_examples_filenames_vector)
+function oximetry_linear_regression(training_examples_filenames_vector, feature_vector_size)
 	training_examples_matrices_array = input_videos(training_examples_filenames_vector)
 
 	training_matrix = zeros(0,0)
 	for training_example_matrix = training_examples_matrices_array
-		features_matrix = features_vectors(training_example_matrix)
+		features_matrix = features_vectors(training_example_matrix, feature_vector_size)
 		cat(1, training_matrix, features_matrix)
 	end
 
